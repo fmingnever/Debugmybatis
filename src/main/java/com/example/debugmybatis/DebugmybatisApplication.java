f88252392b2950e99415f6349286bc86dc6c2a8c
fanming <513792218@qq.com>
2021-01-12 01:42:32 +0800

    'commit'

---
+++ src/main/java/com/example/debugmybatis/DebugmybatisApplication.java
@@ -0,0 +1,14 @@
+package com.example.debugmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebugmybatisApplication {
+
+    public static void main(String[] args) {
+        //开始
+        SpringApplication.run(DebugmybatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/debugmybatis/DebugmybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.debugmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebugmybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
